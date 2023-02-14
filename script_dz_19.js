ac0c9562c2e4e2738549b7eb2730a1fcb674a042
DimaTykva <dimatykvadanait@gmail.com>
2023-02-14 18:27:10 +0200

    update - arguments

--- script_dz_19.js
+++ script_dz_19.js
@@ -1,9 +1,19 @@
 
-function seyHello(...name){
-    if(name.length > 1){
-        alert('Hello guys ' + name)
+// function seyHello(...name){
+//     if(name.length > 1){
+//         alert('Hello guys ' + name)
+//     }else {
+//         alert( 'Hello,' + name)
+//     }
+// }
+
+function seyHello(name){
+    let args = Array.prototype.slice.call(arguments);
+    console.log(args)
+    if(args.length > 1){
+        alert('Hello guys ' + args.join(','))
     }else {
-        alert( 'Hello,' + name)
+        alert( 'Hello,' + args)
     }
 }
 
